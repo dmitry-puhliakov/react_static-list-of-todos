d5f91698f2e466eb97057cce8bfc9aac342faec4
Dmitry Pukhliakov <d.puhliakov@gmail.com>
2021-05-19 20:26:30 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "./precache-manifest.9a5bb4cb80de039cd190f5333cf2374e.js"
 );
 
 self.addEventListener('message', (event) => {
